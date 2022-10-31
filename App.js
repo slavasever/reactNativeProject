3eee740b4f3cb34ac17c36285b9428271395542f
Viacheslav Severyn <sever_2008@ukr.net>
2022-10-31 22:19:36 +0200

    1

--- App.js
+++ App.js
@@ -9,8 +9,86 @@ import {
   Keyboard,
   TouchableOpacity,
 } from "react-native";
+import { useState, useEffect, useCallback } from "react";
+import * as Font from "expo-font";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  // !!!!!!!!!!!! Завантаження шрифтів через хук useFonts !!!!!!!!!!!!
+
+  // const [fontsLoaded] = useFonts({
+  //   "Releway-Italic": require("./assets/fonts/Raleway-Italic-VariableFont_wght.ttf"),
+  //   "Roboto-Italic": require("./assets/fonts/Roboto-Italic.ttf"),
+  //   "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  //   "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  //   "DM-Mono": require("./assets/fonts/DMMono-Regular.ttf"),
+  //   "Inter-Black": require("./assets/fonts/Inter-Black.ttf"),
+  // });
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+
+  // if (!fontsLoaded) {
+  //   return null;
+  // }
+
+  // !!!!!!!!!!!! Завантаження шрифтів через Font.loadAsync !!!!!!!!!!!!!!!!!!!!
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "Releway-Italic": require("./assets/fonts/Raleway-Italic-VariableFont_wght.ttf"),
+          "Roboto-Italic": require("./assets/fonts/Roboto-Italic.ttf"),
+          "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+          "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+          "DM-Mono": require("./assets/fonts/DMMono-Regular.ttf"),
+          "Inter-Black": require("./assets/fonts/Inter-Black.ttf"),
+          "Lobster-Regular": require("./assets/fonts/Lobster-Regular.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  const handleSubmit = () => {
+    const data = {
+      email,
+      password,
+    };
+
+    Keyboard.dismiss();
+    console.log(data);
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={s.container}>
@@ -19,16 +97,34 @@ export default function App() {
           resizeMode="cover"
           style={s.background}
         >
+          <View onLayout={onLayoutRootView}>
+            <Text style={s.header}>Welcome Back!</Text>
+          </View>
           <View style={s.form}>
             <View>
               <Text style={s.inputLabel}>EMAIL</Text>
-              <TextInput style={s.input} textAlign="center" />
+              <TextInput
+                style={s.input}
+                textAlign="center"
+                value={email}
+                onChangeText={(value) => setEmail(value)}
+              />
             </View>
             <View style={{ marginTop: 20 }}>
               <Text style={s.inputLabel}>PASSWORD</Text>
-              <TextInput style={s.input} textAlign="center" secureTextEntry />
+              <TextInput
+                style={s.input}
+                textAlign="center"
+                secureTextEntry
+                value={password}
+                onChangeText={(value) => setPassword(value)}
+              />
             </View>
-            <TouchableOpacity style={s.btn} activeOpacity={0.7}>
+            <TouchableOpacity
+              style={s.btn}
+              activeOpacity={0.7}
+              onPress={handleSubmit}
+            >
               <Text style={s.btnText}>SIGN IN</Text>
             </TouchableOpacity>
           </View>
@@ -50,6 +146,13 @@ const s = StyleSheet.create({
     flex: 1,
     // alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    color: "#fff",
+    textAlign: "center",
+    marginBottom: 80,
+    fontSize: 30,
+    fontFamily: "Lobster-Regular",
   },
   form: {
     marginHorizontal: 40,
